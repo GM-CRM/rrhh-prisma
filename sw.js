e661b673b90bbea9ffa9da7e230f08b4208e4d67
GM-CRM <globalmedia.crm@gmail.com>
2026-07-07 11:39:14 -0600

    expedientes
    reestructura y diseño del modulo.

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 //  Solo usa caché como fallback si no hay red disponible.
 // ============================================================
 
-const CACHE_NAME = 'prisma-rh-v92';
+const CACHE_NAME = 'prisma-rh-v93';
 const ASSETS_ESTATICOS = [
     '/manifest.json',
     '/icon.svg'
